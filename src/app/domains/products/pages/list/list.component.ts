645080ec2bf1a204ad54e3e81ddde6a53cfcb0d6
Edgardo Luis Jimenez <edgardojimenezu@gmail.com>
2024-08-20 13:07:39 -0500

    Get data from a rest api

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -3,6 +3,7 @@ import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -14,39 +15,15 @@ import { CartService } from '../../../shared/services/cart.service';
 export class ListComponent {
   products = signal<Product[]>([]);
   private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
-  constructor() {
-    const initProducts: Product[] = [
-      {
-        id: Date.now(),
-        title: 'Producto 1',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=23',
-        creationAt: new Date().toISOString(),
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
       },
-      {
-        id: Date.now(),
-        title: 'Producto 2',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=32',
-        creationAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now(),
-        title: 'Producto 3',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=15',
-        creationAt: new Date().toISOString(),
-      },
-      {
-        id: Date.now(),
-        title: 'Producto 2',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=11',
-        creationAt: new Date().toISOString(),
-      },
-    ];
-    this.products.set(initProducts);
+      error: () => {},
+    });
   }
 
   addToCart(product: Product) {
